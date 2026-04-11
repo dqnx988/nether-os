bda85cbb5ea071e1cb392e7235f6d72506cb0676
dqnx988 <dan.k.19@outlook.com>
2026-04-11 10:46:39 +0200

    project folders reorganization

--- products/explorer/installuser.ps1
+++
@@ -1,5 +0,0 @@
-$ProductName = "NetherOS Explorer"
-$InstallFolder = "$env:USERPROFILE\AppData\Local\Programs\NetherOS\$ProductName"
-Remove-Item -Path $InstallFolder -Recurse -Force -ErrorAction SilentlyContinue
-New-Item -Path $InstallFolder -ItemType Directory -Force | Out-Null
-Copy-Item -Path "publish\*" -Destination $InstallFolder -Recurse -Force -ErrorAction SilentlyContinue
--- products/ui/installsystem.ps1
+++
@@ -1,5 +0,0 @@
-$ProductName = "NetherOS UI"
-$InstallFolder = "$env:ProgramFiles\NetherOS\$ProductName"
-Remove-Item -Path $InstallFolder -Recurse -Force -ErrorAction SilentlyContinue
-New-Item -Path $InstallFolder -ItemType Directory -Force | Out-Null
-Copy-Item -Path "publish\*" -Destination $InstallFolder -Recurse -Force -ErrorAction SilentlyContinue
