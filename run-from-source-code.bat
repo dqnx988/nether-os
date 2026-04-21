b103a57a727a16097438730ffca93bc6d66fe320
dqnx988 <dan.k.19@outlook.com>
2026-04-21 20:54:54 +0200

    updated run-from-source-code.bat

--- run-from-source-code.bat
+++ run-from-source-code.bat
@@ -1,18 +1,19 @@
 @echo off
-cd /d %~dp0
 
 :loop
+
+cd /d %~dp0
+
 set /p name=Enter project name:
 
 if not exist "source-codes\%name%" (
-    echo Project `%name%` does not exist.
-    echo.
-    goto loop
+echo Project %name% does not exist.
+echo.
+goto loop
 )
 
 cd /d "source-codes\%name%"
 
 dotnet run
 
-cd /d %~dp0
 goto loop
